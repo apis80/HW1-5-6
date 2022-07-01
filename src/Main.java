ffc94edd896e6166b7ae9c4d806fc376983b2db6
apis8 <apis80@mail.ru>
2022-07-01 11:01:43 +0100

    комит

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length -1; i >= 0; i--) {
 
 
-        System.out.println("Hello world!");
+            System.out.print(reverseFullName [i]);
+
+        }
+
+
     }
 }
